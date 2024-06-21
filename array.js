d791f252120dcead6bec437726aeb436cdac5a72
ajay1931 <ajayprakash032@gmail.com>
2024-06-21 16:50:37 +0530

    array file deleted

--- array.js
+++
@@ -1,8 +0,0 @@
-var fruit=["apple","grape","orange","banana","pinapple"]
-
-for(count=0;count<fruit.length;count++){
-    // console.log(fruit[count])
-    // console.log(count)
-    console.log(fruit.length)
-}
-// this is array[]
